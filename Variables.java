48c7004089d1ab34ee2fc401c278f5c2ce4f5fb5
edsonibarra <edsonibarra97@gmail.com>
2022-12-17 02:17:22 -0600

    int variable

---
+++ Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+        int passengers = 4;
+        System.out.println(passengers);
+
+        int busTickets = passengers;
+        System.out.println(busTickets);
+
+    }
+}
+
+// int: 4 bytes
