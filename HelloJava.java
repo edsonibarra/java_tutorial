9cabb7cff617ff22bd5ea6fca57ddb67c65b3c2c
edsonibarra <edsonibarra97@gmail.com>
2022-12-17 02:06:16 -0600

    modified java class

--- HelloJava.java
+++ HelloJava.java
@@ -1,6 +1,5 @@
 public class HelloJava {
     public static void main(String[] args) {    
-        System.out.println("Hello Java");
-        System.out.println("I'm Learning Java Programming Language");
+        System.out.println("Hello Java Programming Language");
     }
 }
